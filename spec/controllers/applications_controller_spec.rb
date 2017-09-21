8f63be4cab860fe874975e50f22a3b1c15539c2d
ror-dev-517 <rubydev517@gmail.com>
2017-09-22 01:35:47 +0530

    unwanted code deleted.

--- spec/controllers/applications_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe ApplicationController, type: :controller do
-  describe 'before actions' do
-    it { is_expected.to use_before_action(:set_cart_in_session) }
-  end
-end
